655bab1094dafe3850f05638d521d5ecaf184931
brianshanehe <37947753+brianshanehe@users.noreply.github.com>
2021-12-29 19:42:03 -0800

    added xqc to tweetbank

--- src/TweetsBank.js
+++ src/TweetsBank.js
@@ -28,6 +28,36 @@ var TweetsBank = {
         "handle": "REALMizkif",
         "text": "I got a fucking toothbrush for Christmas from my mom and she said “you’re a millionaire buy the toothpaste”",
         "datetime": "2:03 PM · Dec 25, 2021"
+    },
+    "1423457101645422593": {
+        "name": "xQc",
+        "handle": "xQc",
+        "text": "YO @kanyewest I LOVE THE MUSIC AND I WANT TO SHARE YOUR ART WITH MY COMMUNITY ON TWITCH. CAN I WATCH/REACT TO DONDA EVENT LIVE ON STREAM?",
+        "datetime": "6:33 PM · Aug 5, 2021"
+    },
+    "1335516744585277441": {
+        "name": "xQc",
+        "handle": "xQc",
+        "text": "TEAM ROCKET UNBOXING WAS CRAZY.  IM SO HAPPY WITH THESE INSANE PULLS, NUTS! THANKS FOR WATCHING! HERE ARE THE HIGHLIGHTS IF U MISSED IT",
+        "datetime": "1:29 AM · Dec 6, 2020"
+    },
+    "1464220525627015172": {
+        "name": "xQc",
+        "handle": "xQc",
+        "text": "MEANINGLESS PHOTO LETTING YOU KNOW I CHANGED HAIR COLOR (FOR PEOPLE WHO DON'T WATCH THE STREAM). ALSO, I'M GRATEFUL FOR EVERYONE WHO WE'RE PART OF THE JOURNEY AND EVEN THOSE WHO HATED SINCE DAY1. + ILL TWEET MORE OFTEN. + RATIO + W",
+        "datetime": "5:12 AM · Nov 26, 2021"
+    },
+    "1457062264247177220": {
+        "name": "xQc",
+        "handle": "xQc",
+        "text": "DOES ANYONE RECOGNIZE ME..? IM KIND OF A BIG DEAL.. HELLO?",
+        "datetime": "12:08 PM · Nov 6, 2021"
+    },
+    "1469636074364305419": {
+        "name": "xQc",
+        "handle": "xQc",
+        "text": "MASTERCHEF IS ONE OF THE MOST ENTERTAINING PIECE OF CONTENT I'VE BINGED IN THE LONGEST TIME AND ALL I EAT IS FAST FOOD, BOOGERS AND TOENAILS. EXPLAIN?",
+        "datetime": "3:51 AM · Dec 11, 2021"
     }
 }
 
